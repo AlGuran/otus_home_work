81ed398b12c1f8837d1285b9cfd8c50be60fb5a5
y.gubkin <y.gubkin@galaksion.com>
2021-03-11 11:22:38 +0300

    HW2 done

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,10 +17,23 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		// user cases
+		{input: "z1", expected: "z"},
+		{input: "", expected: ""},
+		{input: "a", expected: "a"},
+		{input: "я", expected: "я"},
+		{input: "AbC3", expected: "AbCCC"},
+		{input: "nn3", expected: "nnnn"},
+		{input: "\nn\t3", expected: "\nn\t\t\t"},
+		{input: "ё", expected: "ё"},
+		{input: "'2^3&1", expected: "''^^^&"},
+		{input: "-1", expected: "-"},
+		{input: " 3 2 1", expected: "      "},
+		{input: `\10\20\30`, expected: ""},
 	}
 
 	for _, tc := range tests {
@@ -34,7 +47,7 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "0", "9", "0a", "0.00000", "-0.00000"}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
