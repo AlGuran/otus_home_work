4e1074c23c5e98e3b3b593d8482b9798e7c38517
y.gubkin <y.gubkin@galaksion.com>
2021-03-17 03:30:36 +0300

    HW3 done

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,62 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+type wordCount struct {
+	word  string
+	count int
+}
+
+var (
+	maxCount = 10
+	re       = regexp.MustCompile(`,|!|\?|\.|'|"|\*`)
+)
+
+func Top10(s string) (r []string) {
+	if s == "" {
+		return r
+	}
+
+	// prepare string
+	s = strings.ToLower(s)
+	s = strings.ReplaceAll(s, "\n", " ")
+
+	// get count words
+	m := make(map[string]int)
+	splitString := strings.Split(s, " ")
+	for _, word := range splitString {
+		word = strings.TrimSpace(word)
+		word = re.ReplaceAllString(word, "")
+		if word == "" || word == "-" {
+			continue
+		}
+		m[word]++
+	}
+
+	// prepare to sort
+	wcArray := make([]wordCount, 0, len(m))
+	for k, v := range m {
+		wcArray = append(wcArray, wordCount{word: k, count: v})
+	}
+
+	sort.Slice(wcArray, func(i, j int) bool {
+		if wcArray[i].count == wcArray[j].count {
+			return wcArray[i].word < wcArray[j].word
+		}
+		return wcArray[i].count > wcArray[j].count
+	})
+
+	// cut to right size
+	for _, v := range wcArray {
+		r = append(r, v.word)
+		if len(r) == maxCount {
+			break
+		}
+	}
+
+	return r
 }
