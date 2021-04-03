d7c131d6d1073cfa0b554c02938527f33948b0d8
y.gubkin <y.gubkin@galaksion.com>
2021-04-03 22:16:59 +0300

    HW04 done

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,15 +13,14 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	mx       sync.RWMutex
 }
 
 type cacheItem struct {
-	key   string
+	key   Key
 	value interface{}
 }
 
@@ -27,4 +30,49 @@ func NewCache(capacity int) Cache {
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (lc *lruCache) Set(key Key, value interface{}) bool {
+	lc.mx.Lock()
+	defer lc.mx.Unlock()
+
+	item, ok := lc.items[key]
+
+	if !ok {
+		if lc.queue.Len() == lc.capacity {
+			last := lc.queue.Back()
+			lc.queue.Remove(last)
+			delete(lc.items, last.Value.(*cacheItem).key)
+		}
+
+		cacheItem := &cacheItem{key, value}
+		item = lc.queue.PushFront(cacheItem)
+		lc.items[cacheItem.key] = item
+		return false
+	}
+
+	item.Value.(*cacheItem).value = value
+	lc.queue.MoveToFront(item)
+	return true
+}
+
+func (lc *lruCache) Get(key Key) (interface{}, bool) {
+	lc.mx.RLock()
+	defer lc.mx.RUnlock()
+
+	item, ok := lc.items[key]
+
+	if ok {
+		lc.queue.MoveToFront(item)
+		return item.Value.(*cacheItem).value, true
+	}
+
+	return nil, false
+}
+
+func (lc *lruCache) Clear() {
+	lc.mx.Lock()
+	defer lc.mx.Unlock()
+
+	lc.queue, lc.items = NewList(), make(map[Key]*ListItem)
 }
