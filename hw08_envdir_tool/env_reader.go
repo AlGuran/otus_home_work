fa93aaab676209d7235fae656bef29c385fe8e08
y.gubkin <y.gubkin@galaksion.com>
2021-11-28 23:58:29 +0300

    HW08 Done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"bufio"
+	"errors"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +20,33 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := make(Environment)
+	for _, f := range files {
+		if strings.Contains(f.Name(), "=") {
+			continue
+		}
+
+		file, err := os.Open(dir + "/" + f.Name())
+		if err != nil {
+			return nil, err
+		}
+
+		reader := bufio.NewReader(file)
+		str, err := reader.ReadString('\n')
+		if err != nil && !errors.Is(err, io.EOF) {
+			return nil, err
+		}
+
+		str = strings.TrimRight(str, "\n\t ")
+		str = strings.ReplaceAll(str, "\x00", "\n")
+
+		env[f.Name()] = EnvValue{Value: str}
+	}
+
+	return env, nil
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	var argLen int = 3
+
+	if len(os.Args) < argLen {
+		log.Fatal("Too few arguments")
+	}
+
+	var directory string = os.Args[1]
+	var envArgs []string = os.Args[2:]
+
+	env, err := ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(RunCmd(envArgs, env))
 }
